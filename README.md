c0b2872668db688611e0664bcc67a9510d91e7a1
Jen B <jenbutler984@gmail.com>
2023-04-15 10:42:19 -0400

    Working on save button feature to save to local storage

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ https://blog.terresquall.com/2022/09/fixing-the-e-indexof-is-not-a-function-erro
 https://stackoverflow.com/questions/4931825/regular-expression-any-number
 
 https://stackoverflow.com/questions/75718279/im-trying-to-compare-time-using-dayjs-as-well-as-jquery
+
+https://stackoverflow.com/questions/72512032/how-can-i-compare-the-current-time-with-my-divs-id-900-in-javascript
+
+https://www.geeksforgeeks.org/jquery-click-method/
+
+https://www.geeksforgeeks.org/how-to-select-id-that-starts-with-certain-character-using-jquery/#
+
+https://www.geeksforgeeks.org/how-to-send-row-data-when-clicking-button-using-javascript/
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,23 +13,26 @@
 // THEN the saved events persist
 
 $(window).on("load",function(e) {
+
 $(function () {
 
-  // TODO: Add a listener for click events on the save button. This code should
+    // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   
-  var saveBtn = $('.saveBtn');
-  saveBtn.on('click', '.saveBtn', saveCalendarText);
+  $('.saveBtn').on('click', function(event){
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+    var rowId = event.currentTarget.parentNode.id;
+    console.log(rowId);
+    var rowTextContentArea = $('#' + rowId).find("textarea");
+    console.log(rowTextContentArea);
+    var rowTextContent = rowTextContentArea.val();
+    console.log(rowTextContent);
+
+  })
 
   function changeEventColor() {
 
@@ -41,7 +44,6 @@ $(function () {
       var id = timeBlock.id;
       var idHourMatch = id.match(/[0-9]+/);
       var idHour = parseInt(idHourMatch, 10);
-      console.log("id hour " + idHour);
   
       if (idHour < currentHour) {
         $(timeBlock).addClass('past');
@@ -61,11 +63,12 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
 
-  function saveCalendarText(event) {
-    console.log("something");
-  }
 
-  // TODO: Add code to display the current date in the header of the page.
+
+
+
+
+
   function displayTime() {
     var dateDisplayEl = $('#currentDay');
     var rightNow = dayjs().format('dddd, MMM DD, YYYY [at] hh:mm:ss a');
@@ -74,5 +77,5 @@ $(function () {
   displayTime();
   setInterval(displayTime, 1000);
 
-});
+  });
 });
